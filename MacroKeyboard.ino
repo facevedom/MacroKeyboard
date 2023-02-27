bae85ca882275f97d294be47f4b48204c6803764
Felipe Acevedo <felipe.a.montoya@gmail.com>
2023-02-26 21:49:00 -0500

    Add working BLE Keyboard example

--- MacroKeyboard.ino
+++ MacroKeyboard.ino
@@ -1,12 +1,23 @@
+/**
+ * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
+ */
+#include <BleKeyboard.h>
+
+BleKeyboard bleKeyboard("Pipe MacroKB", "Felipe Acevedo", 100);
+
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:'
-  Serial.println("Looping.");
-  delay(2000);
+  if (bleKeyboard.isConnected())
+  {
+    Serial.println("Sending 'Hello world'...");
+    bleKeyboard.print("Hello world");
+    delay(5000);
+  }
 }
