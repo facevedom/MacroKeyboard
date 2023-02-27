361617a8784eefbf77393ab1adea9406d3f06870
Felipe Acevedo <felipe.a.montoya@gmail.com>
2023-02-26 22:11:03 -0500

    Add calibration function

--- MacroKeyboard.ino
+++ MacroKeyboard.ino
@@ -1,15 +1,45 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
+#include "FS.h"
+#include <SPI.h>
+#include <TFT_eSPI.h>
 #include <BleKeyboard.h>
 
+TFT_eSPI tft = TFT_eSPI();
+
+// This is the file name used to store the calibration data
+#define CALIBRATION_FILE "/TouchCalData2"
+
+// Set REPEAT_CAL to true instead of false to run calibration
+// again, otherwise it will only be done once.
+// Repeat calibration if you change the screen rotation.
+#define REPEAT_CAL false
+
 BleKeyboard bleKeyboard("Pipe MacroKB", "Felipe Acevedo", 100);
+
+#define PRIMARY_COLOR 0x000F // 0x000F //TFT_NAVY
+#define ACCENT_COLOR 0x001F  // 0x001F //TFT_BLUE
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   bleKeyboard.begin();
+
+  // Initialise the TFT screen
+  tft.init();
+
+  // Set the rotation before we calibrate
+  tft.setRotation(1);
+
+  // Calibrate the touch screen and retrieve the scaling factors
+  touch_calibrate();
+
+  // Clear the screen
+  tft.fillScreen(TFT_BLACK);
+
+  // Draw keypad background
+  // tft.fillRectVGradient(0, 0, 480, 320, ACCENT_COLOR, PRIMARY_COLOR);
+  tft.fillRect(0, 0, 480, 100, ACCENT_COLOR);
+  tft.fillRect(0, 100, 480, 220, PRIMARY_COLOR);
 }
 
 void loop()
@@ -17,7 +47,80 @@ void loop()
   if (bleKeyboard.isConnected())
   {
     Serial.println("Sending 'Hello world'...");
-    bleKeyboard.print("Hello world");
+    // bleKeyboard.print("Hello world");
     delay(5000);
+  }
+}
+
+void touch_calibrate()
+{
+  uint16_t calData[5];
+  uint8_t calDataOK = 0;
+
+  // check file system exists
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Formating file system");
+    SPIFFS.format();
+    SPIFFS.begin();
+  }
+
+  // check if calibration file exists and size is correct
+  if (SPIFFS.exists(CALIBRATION_FILE))
+  {
+    if (REPEAT_CAL)
+    {
+      // Delete if we want to re-calibrate
+      SPIFFS.remove(CALIBRATION_FILE);
+    }
+    else
+    {
+      File f = SPIFFS.open(CALIBRATION_FILE, "r");
+      if (f)
+      {
+        if (f.readBytes((char *)calData, 14) == 14)
+          calDataOK = 1;
+        f.close();
+      }
+    }
+  }
+
+  if (calDataOK && !REPEAT_CAL)
+  {
+    // calibration data valid
+    tft.setTouch(calData);
+  }
+  else
+  {
+    // data not valid so recalibrate
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(20, 0);
+    tft.setTextFont(2);
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    tft.println("Touch corners as indicated");
+
+    tft.setTextFont(1);
+    tft.println();
+
+    if (REPEAT_CAL)
+    {
+      tft.setTextColor(TFT_RED, TFT_BLACK);
+      tft.println("Set REPEAT_CAL to false to stop this running again!");
+    }
+
+    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.println("Calibration complete!");
+
+    // store data
+    File f = SPIFFS.open(CALIBRATION_FILE, "w");
+    if (f)
+    {
+      f.write((const unsigned char *)calData, 14);
+      f.close();
+    }
   }
 }
